9443f9e0e087f0e569cd76668416b1577bc35f97
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-06-11 14:10:48 +0100

    Testing concatenations with a button in js.

--- js/test.js
+++ js/test.js
@@ -1,1 +1,9 @@
-let isGreater = 1 > 2;
+
+const button = document.querySelector("button");
+
+function greet() {
+    const name = prompt("What is your name?");
+    alert(`Hello ${name}, nice to see you!`);
+}
+
+button.addEventListener("click", greet);

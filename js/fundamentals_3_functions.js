8c157c68468f05f85e0af487a756a1cf039f46a1
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-06-18 13:37:36 +0100

    Finished assignments for fundamentals part 3.

--- js/fundamentals_3_functions.js
+++ js/fundamentals_3_functions.js
@@ -6,10 +6,22 @@ function multiply(a, b) {
     return a * b;
 }
 
-function capitalise() {
+function capitalise(string) {
+
+    let firstLetter = string.slice(0, 1);
+
+    let firstLetterUpper = firstLetter.toUpperCase();
+
+    let restString = string.slice(1, );
+
+    let restStringLower = restString.toLowerCase();
+
+    return firstLetterUpper + restStringLower;
     
 }
 
-function lastLetter() {
-    
+function lastLetter(string) {
+    let lastLetter = string.slice(-1);
+
+    return lastLetter;
 }
